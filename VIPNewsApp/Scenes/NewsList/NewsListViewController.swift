7dbcff9527b98ec0998723c24c27a04a3c4c41ff
Onuralp <onuralp.yahnici@huawei.com>
2022-11-07 15:18:46 +0300

    Remove unused codes

--- VIPNewsApp/Scenes/NewsList/NewsListViewController.swift
+++ VIPNewsApp/Scenes/NewsList/NewsListViewController.swift
@@ -53,18 +53,6 @@ class NewsListViewController: UIViewController, NewsListDisplayLogic
     router.dataStore = interactor
   }
   
-  // MARK: Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
-    }
-  }
-  
   override func viewDidLoad()
   {
     super.viewDidLoad()
