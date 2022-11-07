e8cc0298b806a76f697b29156a82ccb3b4d2b451
Onuralp <onuralp.yahnici@huawei.com>
2022-11-07 17:00:37 +0300

    Create configuration in NewsCell for NewsListVC

--- VIPNewsApp/Scenes/NewsList/NewsCell/NewsCell.swift
+++ VIPNewsApp/Scenes/NewsList/NewsCell/NewsCell.swift
@@ -12,4 +12,11 @@ class NewsCell: UITableViewCell {
     @IBOutlet weak var newsLabelField: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var heartImageView: UIImageView!
+    
+    func configureWithNew(withNew: Article) {
+        newLabelField.text = withNew.title
+        guard let imageUrl = withNew.urlToImage else {return}
+        guard let urlString = URL(string: imageUrl) else {return}
+        newImageView.sd_setImage(with: urlString)
+    }
 }
