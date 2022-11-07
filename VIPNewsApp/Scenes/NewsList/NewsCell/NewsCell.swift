39b988ffcc1f79b15c82db9d7eec524e55d7920c
Onuralp <onuralp.yahnici@huawei.com>
2022-11-07 17:02:33 +0300

    Add injecting SDWebImage and change image name

--- VIPNewsApp/Scenes/NewsList/NewsCell/NewsCell.swift
+++ VIPNewsApp/Scenes/NewsList/NewsCell/NewsCell.swift
@@ -6,17 +6,18 @@
 //
 
 import UIKit
+import SDWebImage
 
 class NewsCell: UITableViewCell {
-    @IBOutlet weak var newImageView: UIImageView!
+    @IBOutlet weak var newsImageView: UIImageView!
     @IBOutlet weak var newsLabelField: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var heartImageView: UIImageView!
     
     func configureWithNew(withNew: Article) {
-        newLabelField.text = withNew.title
+        newsLabelField.text = withNew.title
         guard let imageUrl = withNew.urlToImage else {return}
         guard let urlString = URL(string: imageUrl) else {return}
-        newImageView.sd_setImage(with: urlString)
+        newsImageView.sd_setImage(with: urlString)
     }
 }
